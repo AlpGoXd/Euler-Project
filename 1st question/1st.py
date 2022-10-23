0bce0fd6093193be2bf1708ef470d38c220a855a
Alp Gökalp <alpgokalp@hotmail.com.tr>
2022-10-23 17:06:37 +0300

    1 soru
    1. soru bitti

---
+++ 1st question/1st.py
@@ -0,0 +1,18 @@
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+while 1000 > b*3:
+    a = a+b*3
+    b = b+1
+print("------------")
+while 1000 > d*5:
+    c = c+d*5
+    d = d+1
+while 1000 > f*15:
+    e = e+f*15
+    f = f+1
+print(e)
+print(a+c-e)
