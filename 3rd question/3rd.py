1a660725d3ccc09b92e489a0df8511e561282731
Alp Gökalp <alpgokalp@hotmail.com.tr>
2022-10-24 15:22:03 +0300

    3. soru bitti

--- 3rd question/3rd.py
+++ 3rd question/3rd.py
@@ -1,20 +1,23 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 number = 600851475143
-#TAKES TOO LONG
-
+# FIXED :D
 def den(x):
     a = 0
     set = []
-    while x > a + 1:
+    while x % 2 == 0:
+        x = x/2
+        set.append(2)
+    while x >= a + 1:
         a = a + 1
-        print(a)
-        if x / a == x // a:
+        if x % a == 0:
             set.append(a)
+            x = x/a
     return set
 
 
 def prime(x):
+    mek = list()
     for y in x:
         a = 1
         b = int
@@ -23,7 +26,7 @@ def prime(x):
             if y / a == y // a:
                 b = 1
         if b != 1:
-            print(y)
-
+            mek.append(y)
+    return mek
 
 print(prime(den(number)))
