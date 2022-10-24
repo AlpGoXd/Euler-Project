59e969b51dcd5d93fc600014cfc90024e4a3f136
Alp Gökalp <alpgokalp@hotmail.com.tr>
2022-10-24 15:43:55 +0300

    3. soru bitti

--- 3rd question/3rd.py
+++ 3rd question/3rd.py
@@ -1,6 +1,6 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
-number = 6008514751777
+number = 600851475143
 # FIXED :D
 def fact(x):
     a = 0
