ca74aff1f0367eedaa5df20006c9e81f81c0d3c1
Alp Gökalp <alpgokalp@hotmail.com.tr>
2022-11-11 22:17:33 +0300

    31. SORU DA BİTTİ SONUNDA !!!!!!

--- 31th question/31th.py
+++ 31th question/31th.py
@@ -1,21 +1,22 @@
 # In the United Kingdom the currency is made up of pound (£) and pence (p). There are eight coins in general
 # circulation: 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p). How many ways can £2 be made using any
 # number of coins?
+# HARDEST ONE SO FAR REQUIRES
+# UNDERSTANDING DYNAMIC PROGRAMMING
 
-def fact(x):
-    a = 1
-    for i in range(1, x + 1):
-        a *= i
-    return a
+coins = [2, 5, 10, 20, 50, 100, 200]
 
 
-def comb(x, y):
-    if y > x:
-        print("combination error")
-        return 0
-    a = fact(x) / (fact(y) * fact(x - y))
-    return int(a)
+def sus(x, y):
+    amogus = []
+    for i in range(y + 1):
+        amogus.append(1)
+    for j in x:
+        for k in range(y + 1):
+            if k >= j:
+                amogus[k] += amogus[k - j]
+    print(amogus)
+    return amogus[y]
 
 
-pra = [1, 2, 5, 10, 20, 50, 100, 200]
-t = 200
+print(sus(coins, 200))
